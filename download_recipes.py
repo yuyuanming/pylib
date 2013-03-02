3a1ea3a008fe2c504ce0d10d57e4df51680a1741
Yuanming Yu <yyming@gmail.com>
2013-03-02 17:40:55 +0800

    script to download recipes of python
    multi-thread and single-thread version

---
+++ download_recipes.py
@@ -0,0 +1,84 @@
+import urllib2
+import bs4
+from os import path, makedirs
+
+import threading
+import Queue
+import time
+    
+base_url = 'http://code.activestate.com'
+recipe_base_url = 'http://code.activestate.com/recipes/langs/python/?page='
+recpie_suffix_url = '/download/1/'
+folder = 'recipes'
+total_page = 199 # keep it up-to-date 
+cnt = 0
+minitask = 5
+
+class store_recipe(threading.Thread):
+    def __init__(self, q):
+        threading.Thread.__init__(self)
+        self.q = q 
+
+    def run(self):
+        try:
+            url = self.q.get()
+            url = url.split('?')[0]
+
+            fn = folder + '/' + url[9:-1]+'.py'
+            #if path.exists(fn): return
+
+            html = urllib2.urlopen(base_url + url + recpie_suffix_url).read()
+            fout = open(fn, 'w')
+            fout.write(html)
+            global cnt
+        except:
+            print " RE-TRYING ",
+            cnt = cnt - 1
+            self.q.put(url)
+            self.run()
+        finally:
+            cnt = cnt + 1
+            print str(cnt)+"(" + str(threading.active_count())+")", url
+            fout.close()
+            self.q.task_done()
+
+def handle_page(page):
+    print 'Start downloading page ' + str(page) + '.'
+
+    # get recpies list on current page
+    html = urllib2.urlopen(recipe_base_url + str(page)).read()
+    soup = bs4.BeautifulSoup(html)
+    recipes = [content.a['href'] 
+            for content in soup.findAll(attrs={'class':'recipe-title'})]
+    
+    q = Queue.Queue()
+    for k in xrange(0, len(recipes), minitask):
+        for i in range(minitask):
+            t = store_recipe(q)
+            t.setDaemon(True)
+            t.start()
+        for i in range(minitask):
+            q.put(recipes[k+i])
+        q.join()
+        q.queue.clear()
+        del t
+    del q
+
+    #print recipes
+    '''
+    for url in recipes:
+        store_recipe(url)
+        t = threading.Thread(target=store_recipe, args=(url))
+        t.daemon = True
+        t.start()
+        '''
+
+    print 'Done page ' + str(page) + '.'
+    print
+
+if __name__=='__main__':
+    if not path.exists('recipes'): 
+        makedirs('recipes')
+    handle_page(1)
+    for i in range(1, 10):
+        handle_page(i)
---
+++ download_recipes_single.py
@@ -0,0 +1,53 @@
+import urllib2
+import bs4
+from os import path, makedirs
+    
+base_url = 'http://code.activestate.com'
+recipe_base_url = 'http://code.activestate.com/recipes/langs/python/?page='
+recpie_suffix_url = '/download/1/'
+folder = 'recipes'
+total_page = 199 # keep it up-to-date 
+cnt = 0
+minitask = 5
+
+def store_recipe(url):
+    try:
+        url = url.split('?')[0]
+        fn = folder + '/' + url[9:-1]+'.py'
+        #if path.exists(fn): return
+
+        html = urllib2.urlopen(base_url + url + recpie_suffix_url).read()
+        fout = open(fn, 'w')
+        fout.write(html)
+        global cnt
+    except:
+        print " RE-TRYING ",
+        cnt = cnt - 1
+        store_recipe(url)
+    finally:
+        cnt = cnt + 1
+        #print str(cnt)+"(" + str(threading.active_count())+")", url
+        print str(cnt), url
+        fout.close()
+
+def handle_page(page):
+    print 'Start downloading page ' + str(page) + '.'
+
+    # get recpies list on current page
+    html = urllib2.urlopen(recipe_base_url + str(page)).read()
+    soup = bs4.BeautifulSoup(html)
+    recipes = [content.a['href'] 
+            for content in soup.findAll(attrs={'class':'recipe-title'})]
+    
+    #print recipes
+    for url in recipes:
+        store_recipe(url)
+
+    print 'Done page ' + str(page) + '.'
+    print
+
+if __name__=='__main__':
+    if not path.exists('recipes'): 
+        makedirs('recipes')
+    for i in range(1, 10):
+        handle_page(i)
